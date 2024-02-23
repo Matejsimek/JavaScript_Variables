e5f549b1700af4d819d8826d3829f0e876941ded
Matej Simek <matisis135@gmail.com>
2024-02-23 11:36:54 -0500

    work copmleted

--- app.js
+++ app.js
@@ -3,8 +3,40 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Matej";
+let lastName = "Simek";
+var age = 21;
 
-// Exercise 2
+lastName = "Simek";
+age = 21;
 
-// Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(
+  `${language} was first release in ${createdYear}.`
+);
+
+let price = 1999;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(
+  `Size: ${selectedSize}; -price: ${price}; -currently available in stock: ${stock}.`
+);
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfusss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true; 
+
+console.log(
+  `${title} by ${author} is ${pageCount} pages long.Your last bookmark was page ${bookmark}.`
+);
+
+
